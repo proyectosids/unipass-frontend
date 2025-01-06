a421cb09e0642dd69226c604280478dcc5c7168a
IRYA-DEV <iyael117@gmail.com>
2025-01-06 16:05:07 -0600

    PUNTO DE RESPALDO ANTES DE IMPLEMENTAR FIREBASE

--- lib/config/config_url.dart
+++ lib/config/config_url.dart
@@ -1,9 +1,9 @@
 // config.dart
 
-// Ruta de API de Base de Datos UniPass
+// Ruta de API de Base de Datos UniPass LOCAL
 const String baseUrl = 'http://10.30.1.235:3000';
 
-// Ruta de API para consumir informacion de la institucion
+// Ruta de API para consumir informacion de la institucion LOCAL
 const String endpointUrl = 'http://10.30.1.235:3002';
 
 const String otpUrl = 'https://api-otp.app.syswork.online';
